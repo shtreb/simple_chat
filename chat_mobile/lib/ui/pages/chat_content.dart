6150728a3d26215a0eb89e656b698074768edf01
Кирилл Штреблев <shtreblevdev@gmail.com>
2020-10-15 22:35:34 +0300

    - add route_aware_widget.dart - add local notification-service.dart - add gen_lang

--- chat_mobile/lib/ui/pages/chat_content.dart
+++ chat_mobile/lib/ui/pages/chat_content.dart
@@ -1,16 +1,16 @@
 import 'dart:async';
 import 'dart:collection';
 
-import 'package:chat_mobile/ui/widgets/items/item-bubble.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_api_client/chat_api_client.dart';
 
 import 'package:chat_models/chat_models.dart';
-import 'package:chat_mobile/ui/widgets/common_ui.dart';
+import 'package:chat_mobile/ui/widgets/items/item-bubble.dart';
 import 'package:chat_mobile/data/cases/api_client.dart';
 import 'package:chat_mobile/data/cases/chat_component.dart';
 import 'package:chat_mobile/flavors/globals.dart' as globals;
+import 'package:chat_mobile/generated/i18n.dart';
 
 class ChatContentPage extends StatefulWidget {
   ChatContentPage({Key key, @required this.chat, @required this.chatComponent})
@@ -30,9 +30,11 @@ class _ChatContentPageState extends State<ChatContentPage> {
   StreamSubscription<Message> _messagesSubscription;
   StreamSubscription<Set<ChatId>> _unreadMessagesSubscription;
   Set<ChatId> _unreadChats = HashSet<ChatId>();
+  ScrollController scrollCtrl;
 
   @override
   void initState() {
+    scrollCtrl = ScrollController();
     super.initState();
     _title = widget.chat.members
         .where((user) => user.id != globals.currentUser.id)
@@ -40,20 +42,9 @@ class _ChatContentPageState extends State<ChatContentPage> {
         .join(", ");
     refreshChatContent();
 
-    _messagesSubscription =
-        widget.chatComponent.subscribeMessages((receivedMessage) {
-      setState(() {
-        _messages.add(receivedMessage);
-      });
+    _messagesSubscription = widget.chatComponent.subscribeMessages((receivedMessage) {
+      setState(() => _messages.add(receivedMessage));
     }, widget.chat.id);
-
-    _unreadMessagesSubscription = widget.chatComponent
-        .subscribeUnreadMessagesNotification((unreadChatIds) {
-      setState(() {
-        _unreadChats.clear();
-        _unreadChats.addAll(unreadChatIds);
-      });
-    });
   }
 
   @override
@@ -66,11 +57,8 @@ class _ChatContentPageState extends State<ChatContentPage> {
 
   void refreshChatContent() async {
     try {
-      List<Message> msgList =
-          await MessagesClient(MobileApiClient()).read(widget.chat.id);
-      setState(() {
-        _messages = msgList;
-      });
+      List<Message> msgList = await MessagesClient(MobileApiClient()).read(widget.chat.id);
+      setState(() => _messages = msgList.reversed.toList());
     } on Exception catch (e) {
       print('Failed to get list of messages');
       print(e);
@@ -78,92 +66,80 @@ class _ChatContentPageState extends State<ChatContentPage> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    var actions = <Widget>[];
-    if (_unreadChats.isNotEmpty) {
-      actions.add(IconButton(
-          icon: Icon(Icons.message),
-          tooltip: 'New messages',
-          color: Colors.greenAccent,
-          onPressed: () {
-            Navigator.popUntil(context, ModalRoute.withName('/tabs'));
-          }));
-    }
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_title),
-        actions: actions,
-      ),
-      body: SafeArea(
-        bottom: true,
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: ListView.builder(
-                padding: EdgeInsets.symmetric(vertical: 24, horizontal: 16),
-                physics: BouncingScrollPhysics(),
-                itemCount: _messages.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return _buildListTile(_messages[index]);
-                },
-              ),
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: Text(_title),
+    ),
+    body: SafeArea(
+      bottom: true,
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              reverse: true,
+              controller: scrollCtrl,
+              padding: EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+              physics: BouncingScrollPhysics(),
+              itemCount: _messages.length,
+              itemBuilder: (BuildContext context, int index) {
+                return _buildListTile(_messages[index]);
+              },
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.symmetric(horizontal: 24),
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(24),
-                          border: Border.all(color: Theme.of(context).primaryColorLight.withOpacity(.5), width: .5),
-                          boxShadow: <BoxShadow> [
-                            BoxShadow(
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 24),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(24),
+                        border: Border.all(color: Theme.of(context).primaryColorLight.withOpacity(.5), width: .5),
+                        boxShadow: <BoxShadow> [
+                          BoxShadow(
                               color: Theme.of(context).primaryColor.withOpacity(.1),
                               blurRadius: 6
-                            )
-                          ]
-                      ),
-                      child: TextField(
-                        controller: _sendMessageTextController,
-                        maxLines: 1,
-                        decoration: InputDecoration(
-                          hintText: 'Your message',
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide.none,
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide.none,
-                          ),
+                          )
+                        ]
+                    ),
+                    child: TextField(
+                      controller: _sendMessageTextController,
+                      maxLines: 1,
+                      decoration: InputDecoration(
+                        hintText: S.of(context).chat_enter_message,
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide.none,
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide.none,
                         ),
                       ),
                     ),
                   ),
-                  const Padding(padding: EdgeInsets.only(left: 8)),
-                  SizedBox(
-                    height: 48,
-                    child: FloatingActionButton(
-                      heroTag: 'sendMessage',
-                      splashColor: Colors.transparent,
-                      child: const Icon(Icons.send),
-                      onPressed: () {
-                        if (_sendMessageTextController.text.isNotEmpty)
-                          send(_sendMessageTextController.text);
-                      },
-                    ),
-                  )
-                ],
-              ),
+                ),
+                const Padding(padding: EdgeInsets.only(left: 8)),
+                SizedBox(
+                  height: 48,
+                  child: FloatingActionButton(
+                    heroTag: 'sendMessage',
+                    splashColor: Colors.transparent,
+                    child: const Icon(Icons.send),
+                    onPressed: () {
+                      if (_sendMessageTextController.text.isNotEmpty)
+                        send(_sendMessageTextController.text);
+                    },
+                  ),
+                )
+              ],
             ),
-            const Padding(padding: EdgeInsets.only(bottom: 16))
-          ],
-        ),
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 16))
+        ],
       ),
-    );
-  }
+    ),
+  );
 
   Widget _buildListTile(Message message) {
     var isMyMessage = message.author.id == globals.currentUser.id;
